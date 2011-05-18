31f4b20cd13d24deac6f00189b738dae45265ca7
danx0r <danbmil99@gmail.com>
2011-05-18 00:23:45 -0700

    subway tiling

--- tile2.js
+++ tile2.js
@@ -3,10 +3,8 @@
  */
 
 TRI5=0;
-DECTRI=1;
+DECTRI=0;
 DECSQ=0;
-REV1=0;
-REV2=1;
 SIZE=1000;
 
 RZ = (1/3) / (1/3 + 3)
@@ -74,7 +72,6 @@ function poly(verts, color){
     
     // crux of matter: return 5 sub-polys acc to pinwheel logic
     this.subdivide = function(){
-    
         // if triangle...
         if (this.verts.length == 3) {
         	var V0 = this.verts[0];
@@ -84,49 +81,67 @@ function poly(verts, color){
         	var B = avg(V1, V2, 0.5);
         	var C = avg(V2, V0, 0.5);
         	var t1 = new poly([V0, A, C], this.color);
-        	var t2 = new poly([C, B, V2], this.color);
+        	var t2 = new poly([C, A, B, V2], this.color);
+        	t2.rect = 1;
         	var t3 = new poly([A, V1, B], this.color);
-        	var t4 = new poly([B, C, A], this.color);
-            return new plist([t1, t2, t3, t4]);
+        	//var t4 = new poly([B, C, A], this.color);
+            return new plist([t1, t2, t3]);
         }
         // if square.. (assume clockwise)
         if (this.verts.length == 4) {
-        	var V0 = this.verts[0];
-        	var V1 = this.verts[1];
-        	var V2 = this.verts[2];
-        	var V3 = this.verts[3];
+	    	var V0 = this.verts[0];
+	    	var V1 = this.verts[1];
+	    	var V2 = this.verts[2];
+	    	var V3 = this.verts[3];
+        	if (this.rect) {
+		        var A = avg(V0, V1, 2.0/4.0);
+		        var B = avg(V1, V2, 2.0/4.0);
+		        var C = avg(V2, V3, 2.0/4.0);
+		        var D = avg(V3, V0, 2.0/4.0);
+		        var E = avg(A, C, 0.5);
+		        var sq1 = new poly([V0, A, E, D], this.color);
+		        sq1.rect=1
+		        var sq2 = new poly([A, V1, B, E], this.color);
+		        sq2.rect=1
+		        var sq3 = new poly([D, E, C, V3], this.color);
+		        sq3.rect=1
+		        var sq4 = new poly([E, B, V2, C], this.color);
+		        sq4.rect=1
+		        return new plist([sq1, sq2, sq3, sq4]);
+        	}
+        	else {
+		        var A = avg(V0, V1, 3.0/4.0);
+		        var B = avg(V1, V2, 3.0/4.0);
+		        var C = avg(V2, V3, 3.0/4.0);
+		        var D = avg(V3, V0, 3.0/4.0);
 
-            var A = avg(V0, V1, 3.0/4.0);
-            var B = avg(V1, V2, 3.0/4.0);
-            var C = avg(V2, V3, 3.0/4.0);
-            var D = avg(V3, V0, 3.0/4.0);
+				var E3 = avg(A, D, 1/3);
+				var E = avg(E3, B, RZ);
 
-			var E3 = avg(A, D, 1/3);
-			var E = avg(E3, B, RZ);
+				var F3 = avg(B, A, 1/3);
+				var F = avg(F3, C, RZ);
 
-			var F3 = avg(B, A, 1/3);
-			var F = avg(F3, C, RZ);
+				var G3 = avg(C, B, 1/3);
+				var G = avg(G3, D, RZ);
 
-			var G3 = avg(C, B, 1/3);
-			var G = avg(G3, D, RZ);
+				var H3 = avg(D, C, 1/3);
+				var H = avg(H3, A, RZ);
 
-			var H3 = avg(D, C, 1/3);
-			var H = avg(H3, A, RZ);
+		        var t1 = new poly([D, A, V0], this.color)
+		        var t2 = new poly([D, A, H], this.color)
 
-            var t1 = new poly([D, A, V0], this.color)
-            var t2 = new poly([D, A, H], this.color)
+		        var t3 = new poly([A, B, V1], this.color)
+		        var t4 = new poly([A, B, E], this.color)
 
-            var t3 = new poly([A, B, V1], this.color)
-            var t4 = new poly([A, B, E], this.color)
+		        var t5 = new poly([B, C, V2], this.color)
+		        var t6 = new poly([B, C, F], this.color)
 
-            var t5 = new poly([B, C, V2], this.color)
-            var t6 = new poly([B, C, F], this.color)
+		        var t7 = new poly([C, D, V3], this.color)
+		        var t8 = new poly([C, D, G], this.color)
 
-            var t7 = new poly([C, D, V3], this.color)
-            var t8 = new poly([C, D, G], this.color)
-
-            var sq = new poly([E, F, G, H], this.color);
-            return new plist([t1, t2, t3, t4, t5, t6, t7, t8, sq]);
+		        var sq = new poly([E, F, G, H], this.color);
+		        return new plist([t1, t2, t3, t4, t5, t6, t7, t8, sq]);
+		    }
         }
         else {
             alert("can't subdivide except tri's and squares")
